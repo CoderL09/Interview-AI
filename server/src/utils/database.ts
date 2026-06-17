f588885dbbbf33ec5122b293585e4c0db9e3b6b7
coderL09 <qndydxx@gmail.com>
2026-06-18 01:09:14 +0800

    fix:建立云端数据库

--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -12,7 +12,11 @@ const pool = mysql.createPool({
   idleTimeout: 600000,
   queueLimit: 0,
   enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+  keepAliveInitialDelay: 0,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  ssl: {
+    rejectUnauthorized: false // 关键代码：跳过证书检查，防止 Render 连不上
+  }
 })
 
 export default pool
